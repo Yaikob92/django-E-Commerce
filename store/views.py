d04c360b230999cb1a6eaeaa4eae78342e63bcce
Yaikob Wasihun <wyaikob@gmail.com>
2025-05-02 10:53:16 +0300

    Implement conditional "Add to Cart" button on product detail page,

--- store/views.py
+++ store/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Product
 from category.models import Category
+from carts.views import _cart_id
+from carts.models import CartItem
+
 # Create your views here.
 def store(request,category_slug=None):
   categories = None
@@ -20,8 +23,10 @@ def store(request,category_slug=None):
 def product_detail(request,category_slug,product_slug):
   
   single_product = get_object_or_404(Product, category__slug=category_slug,slug = product_slug)
+  in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request),product=single_product).exists()
   
   context = {
-    'single_product':single_product
+    'single_product':single_product,
+    'in_cart':in_cart
   }
   return render(request,"store/product-detail.html",context)

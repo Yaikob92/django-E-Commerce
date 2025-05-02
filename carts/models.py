d1e52a9b1513bf7240d437abf47fdd3a5fa8607b
Yaikob Wasihun <wyaikob@gmail.com>
2025-05-02 10:13:57 +0300

    display messages when the cart_item is empty

--- carts/models.py
+++ carts/models.py
@@ -5,7 +5,9 @@ from store.models import Product
 class Cart(models.Model):
   cart_id = models.CharField(max_length=250)
   date_added = models.DateTimeField(auto_now_add=True)
-
+  
+  
+  
 
 class CartItem(models.Model):
   product = models.ForeignKey(Product,on_delete=models.CASCADE)
@@ -13,5 +15,8 @@ class CartItem(models.Model):
   quantity = models.IntegerField()
   is_active = models.BooleanField(default=True)
 
-  def __str__(self):
-    return self.product
+  def sub_total(self):
+    return self.product.price * self.quantity
+
+  # def __str__(self):
+  #   return self.product
--- store/views.py
+++ store/views.py
@@ -18,10 +18,9 @@ def store(request,category_slug=None):
   }
   return render(request,"store/store.html",context)
 def product_detail(request,category_slug,product_slug):
-  try:
-    single_product = get_object_or_404(Product, category__slug=category_slug,slug = product_slug)
-  except Exception as e:
-    raise e
+  
+  single_product = get_object_or_404(Product, category__slug=category_slug,slug = product_slug)
+  
   context = {
     'single_product':single_product
   }

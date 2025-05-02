bd7db407ae57e76a5196e773cd6b68d51eea3af1
Yaikob Wasihun <wyaikob@gmail.com>
2025-05-02 18:26:50 +0300

    implement contex processor and add paginations

--- store/views.py
+++ store/views.py
@@ -3,6 +3,7 @@ from .models import Product
 from category.models import Category
 from carts.views import _cart_id
 from carts.models import CartItem
+from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
 
 # Create your views here.
 def store(request,category_slug=None):
@@ -10,16 +11,25 @@ def store(request,category_slug=None):
   products = None
   if category_slug != None:
     categories = get_object_or_404(Category,slug = category_slug)
-    products = Product.objects.all().filter(category = categories,is_available=True)
+    products = Product.objects.all().filter(category = categories,is_available=True).order_by('id')
+
+    paginator = Paginator(products,1)
+    page = request.GET.get('page')
+    paged_products = paginator.get_page(page)
     product_count = products.count()
   else:
-    products = Product.objects.all().filter(is_available=True)
+    products = Product.objects.all().filter(is_available=True).order_by('id')
+    paginator = Paginator(products,3)
+    page = request.GET.get('page')
+    paged_products = paginator.get_page(page)
     product_count = products.count()
   context = {
-      'products':products,
+      'products':paged_products,
       'product_count':product_count
   }
   return render(request,"store/store.html",context)
+
+
 def product_detail(request,category_slug,product_slug):
   
   single_product = get_object_or_404(Product, category__slug=category_slug,slug = product_slug)
